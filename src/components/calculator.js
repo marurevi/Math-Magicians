6eefd23dd4f921eb68504e08a7425ded23eda37e
marurevi <maryrev@hotmail.com>
2022-05-25 21:41:22 -0300

    Add new folder and files (setup)

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,29 +7,29 @@ class Calculator extends React.PureComponent {
       <ul className="grid-container">
         <li className="fullspan"><div className="output">0</div></li>
 
-        <li><button className="btn-grey" type="button">AC</button></li>
-        <li><button className="btn-grey" type="button">+/-</button></li>
-        <li><button className="btn-grey" type="button">%</button></li>
-        <li><button className="btn-orange" type="button">/</button></li>
+        <li><button type="button" id="AC" className="btn-grey">AC</button></li>
+        <li><button type="button" id="+/-" className="btn-grey">+/-</button></li>
+        <li><button type="button" id="%" className="btn-grey">%</button></li>
+        <li><button type="button" id="/" className="btn-orange">/</button></li>
 
-        <li><button className="btn-grey" type="button">7</button></li>
-        <li><button className="btn-grey" type="button">8</button></li>
-        <li><button className="btn-grey" type="button">9</button></li>
-        <li><button className="btn-orange" type="button">X</button></li>
+        <li><button type="button" id="7" className="btn-grey">7</button></li>
+        <li><button type="button" id="8" className="btn-grey">8</button></li>
+        <li><button type="button" id="9" className="btn-grey">9</button></li>
+        <li><button type="button" id="X" className="btn-orange">X</button></li>
 
-        <li><button className="btn-grey" type="button">4</button></li>
-        <li><button className="btn-grey" type="button">5</button></li>
-        <li><button className="btn-grey" type="button">6</button></li>
-        <li><button className="btn-orange" type="button">-</button></li>
+        <li><button type="button" id="4" className="btn-grey">4</button></li>
+        <li><button type="button" id="5" className="btn-grey">5</button></li>
+        <li><button type="button" id="6" className="btn-grey">6</button></li>
+        <li><button type="button" id="-" className="btn-orange">-</button></li>
 
-        <li><button className="btn-grey" type="button">1</button></li>
-        <li><button className="btn-grey" type="button">2</button></li>
-        <li><button className="btn-grey" type="button">3</button></li>
-        <li><button className="btn-orange" type="button">+</button></li>
+        <li><button type="button" id="1" className="btn-grey">1</button></li>
+        <li><button type="button" id="2" className="btn-grey">2</button></li>
+        <li><button type="button" id="3" className="btn-grey">3</button></li>
+        <li><button type="button" id="+" className="btn-orange">+</button></li>
 
-        <li className="doblespan"><button className="btn-grey" type="button">0</button></li>
-        <li><button className="btn-grey" type="button">.</button></li>
-        <li><button className="btn-orange" type="button">=</button></li>
+        <li className="doblespan"><button type="button" id="0" className="btn-grey">0</button></li>
+        <li><button type="button" id="." className="btn-grey">.</button></li>
+        <li><button type="button" id="=" className="btn-orange">=</button></li>
       </ul>
     );
   }
---
+++ src/logic/calculate.js
@@ -0,0 +1,15 @@
+/* 
+Implement the calculate.js module
+It should contain one default method: calculate.
+The calculate method should accept a calculator data object 
+and a button name as arguments.
+The calculator data object should contain three properties:
+- total, 
+- next 
+- operation.
+This method should use the buttonName argument to mutate 
+the calculator's data object and return its modified 
+version. For instance if buttonName == "+/-", 
+it should multiply total and next by -1 to make it negative
+ or positive.
+*/
